921c0996daa4640311a4e8cd509479efc9516b6b
BerkUtkuYenisey <berkutkuyenisey@gmail.com>
2019-07-06 20:20:41 +0300

    Logout menu has been added to InstructorEnroll too.

--- app/src/main/java/com/iamhere/iamhere/InstructorEnroll.java
+++ app/src/main/java/com/iamhere/iamhere/InstructorEnroll.java
@@ -1,7 +1,12 @@
 package com.iamhere.iamhere;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.Toast;
@@ -44,6 +49,22 @@ public class InstructorEnroll extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.logout_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.logout) {
+            Intent signInActivityIntent = new Intent(getApplicationContext(), SignInActivity.class);
+            startActivity(signInActivityIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
