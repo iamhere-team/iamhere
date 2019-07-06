f66f74ac908011f81deeabb13f4eb2521fe3ecf8
doruksahin <doruksahin98@gmail.com>
2019-07-06 16:19:07 +0300

    A menu has been created for the MainActivity. Logout menu item has been added to it.(Logout only directs to the SignInActivity, it does not sign out properly yet).

--- app/src/main/java/com/iamhere/iamhere/MainActivity.java
+++ app/src/main/java/com/iamhere/iamhere/MainActivity.java
@@ -1,16 +1,19 @@
 package com.iamhere.iamhere;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+
 
 import com.amazonaws.amplify.generated.graphql.CreateTodoMutation;
 import com.amazonaws.amplify.generated.graphql.ListTodosQuery;
 import com.amazonaws.amplify.generated.graphql.OnCreateTodoSubscription;
-import com.amazonaws.mobile.config.AWSConfiguration;
 import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
 import com.amazonaws.mobileconnectors.appsync.AppSyncSubscriptionCall;
 import com.amazonaws.mobileconnectors.appsync.fetcher.AppSyncResponseFetchers;
@@ -112,6 +115,22 @@ public class MainActivity extends AppCompatActivity {
             Log.i("Completed", "Subscription completed");
         }
     };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.logout_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.logout) {
+            Intent signInActivityIntent = new Intent(getApplicationContext(), SignInActivity.class);
+            startActivity(signInActivityIntent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
 
