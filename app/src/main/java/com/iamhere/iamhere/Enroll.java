20b80a6ba2f43d88f0972643e05093917bbd6311
doruksahin <doruksahin98@gmail.com>
2019-07-06 15:00:52 +0300

    Logout menu has been added to Student's EnrollActivity.

--- app/src/main/java/com/iamhere/iamhere/Enroll.java
+++ app/src/main/java/com/iamhere/iamhere/Enroll.java
@@ -1,7 +1,12 @@
 package com.iamhere.iamhere;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class Enroll extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class Enroll extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enroll);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.logout_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.logout) {
+            Intent signInActivityIntent = new Intent(getApplicationContext(), SignInActivity.class);
+            startActivity(signInActivityIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
